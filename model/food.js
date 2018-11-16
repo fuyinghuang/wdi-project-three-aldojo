7fff77926c56098ce0ceb2c4d30168c64559a05f
Gourgey <joshuagourgey@gmail.com>
2018-11-16 15:23:29 +0000

    added requires in food model

--- model/food.js
+++ model/food.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
+const User = require('./user');
+const Country = require('./country');
 
 // Basic food schema
-  const foodSchema = mongoose.Schema({
-    name: String,
-    description: String,
-    image: String,
-    country: [{ type: mongoose.Schema.ObjectId, ref: Country }],
-    comments: [{
-      text: String,
-      user: { type: mongoose.Schema.ObjectId, ref: User },
-      createdBy: { type: mongoose.Schema.ObjectId, ref: User },
-      votes: [User]
-    }]
-  });
+const foodSchema = mongoose.Schema({
+  name: String,
+  description: String,
+  image: String,
+  country: [{ type: mongoose.Schema.ObjectId, ref: Country }],
+  comments: [{
+    text: String,
+    user: { type: mongoose.Schema.ObjectId, ref: User },
+    createdBy: { type: mongoose.Schema.ObjectId, ref: User },
+    votes: [User]
+  }]
+});
 
 
 const foodModel = mongoose.model('Food', foodSchema);
