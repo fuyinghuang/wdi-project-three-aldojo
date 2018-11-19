e16d5971b15250a8280710a606e2ec5d708c0b8a
Gourgey <joshuagourgey@gmail.com>
2018-11-19 14:29:13 +0000

    src files created

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
+import Router from './config/router'; // NOTE: Don't need the .js
+import './scss/main.scss';
+import mainCtrl from './controllers/mainCtrl';
 
-function Router($stateProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/'
-    });
-}
 
-angular.module('yourAppName', ['ui.router'])
-  .config(Router);
+
+
+angular.module('Aldojo', ['ui.router'])
+  .config(Router)
+  .controller('mainCtrl', mainCtrl)
+  .config(function($authProvider) {
+    $authProvider.signupUrl = '/api/register';
+    $authProvider.loginUrl = '/api/login';
+  });
