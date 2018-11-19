c7ff14c380b0a8ad9b25da458512104a3b1627b4
AlexFrancis95 <alexfrancis@email.com>
2018-11-19 16:59:11 +0000

    Food comments added, bcrypt set up

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
   username: String,
@@ -6,7 +7,13 @@ const userSchema = mongoose.Schema({
   password: String
 });
 
+userSchema.pre('save', function(){
+  this.password = bcrypt.hashSync(this.password, 8);
+});
 
+userSchema.methods.validatePassword = function(attemptedPassword){
+  return bcrypt.compareSync(attemptedPassword, this.password);
+};
 
 const userModel = mongoose.model('User', userSchema);
 module.exports = userModel;
