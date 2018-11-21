86c58d543c1dd8055625a8dd49c0422bedfeeb7b
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-11-21 02:07:00 +0000

    seperate sass file

--- src/controllers/loginCtrl.js
+++ src/controllers/loginCtrl.js
@@ -8,3 +8,21 @@ function loginCtrl($scope, $state, $auth) {
 }
 
 export default loginCtrl;
+
+
+// function loginCtrl($scope, $state, $auth, Flash) {
+//   $scope.handleLogin = function() {
+//     console.log('login!');
+//     $auth.login($scope.user)
+//       .then(result => {
+//         Flash.create('success', result.data.message);
+//
+//         $state.go('countryIndex');
+//       })
+//       .catch(err => {
+//
+//         console.log('error', err);
+//         Flash.create('warning', 'Login failed: ' + err.data.message);
+//       });
+//   };
+// }
